cd9d223a5c35255b59bac240c364f1fc8355ad33
fusae <fusae_js@163.com>
2016-07-17 17:11:34 +0800

    almost done

--- dgut_spider/spiders/getTeacherCourse_spider.py
+++ dgut_spider/spiders/getTeacherCourse_spider.py
@@ -1,3 +1,4 @@
+# -*- coding: gb2312 -*-
 import scrapy
 from scrapy.http import FormRequest
 import json
@@ -6,7 +7,7 @@ from datetime import datetime
 from scrapy.selector import Selector
 from dgut_spider.handlePic import handle
 from dgut_spider.items import DetailProfItem
-import re
+from dgut_spider.items import DetailProfCourseItem
 
 class GetTeacherCourseSpider(scrapy.Spider):
     name = 'TeacherCourse'
@@ -16,6 +17,7 @@ class GetTeacherCourseSpider(scrapy.Spider):
         self.vcodeUrl = 'http://jwxt.dgut.edu.cn/jwweb/sys/ValidateCode.aspx' # second
         self.postUrl = 'http://jwxt.dgut.edu.cn/jwweb/ZNPK/TeacherKBFB_rpt.aspx' # third
         self.findSessionId = None # to save the cookies
+        self.XNXQ = '20151'
 
     def start_requests(self):
         request = scrapy.Request(self.getUrl,
@@ -35,7 +37,7 @@ class GetTeacherCourseSpider(scrapy.Spider):
         yzm = handle(response.body)
         
         yield FormRequest(self.postUrl,
-                formdata={'Sel_XNXQ': '20151',
+                formdata={'Sel_XNXQ': self.XNXQ,
                           'sel_zc': '011',
                           'txt_yzm': yzm,
                           'type': '2'},
@@ -69,28 +71,31 @@ class GetTeacherCourseSpider(scrapy.Spider):
         # parse body data
         sel = Selector(text=body)
 
-        XXNQ = '20151' # test
-
-        # department, teacher, gender and title
-        temp1 = sel.xpath('//*[@group="group"]/table/tr/td/text()').extract() 
-        
-        for each in temp1:
-            each = each.replace(u'\xa0', u'  ')
-            print(each.split('   '))
-
-        # get all the note data by regular expressions
+        # get all the note text data
         noteTables = sel.xpath('//table[@style="border:0px;"]').extract()
+
+        noteList = [] # to store all the note text
         for noteTable in noteTables:
             if '<b>'  in noteTable:
                 sele = Selector(text = noteTable)
                 note = (sele.xpath('//table/tr/td/b/text()').extract())
                 noteText = (sele.xpath('//table/tr/td/text()').extract())
+                # combine note and noteText
+                if not noteText:
+                    noteText.append('')
+                    noteText.append('')
+                else:
+                    if len(noteText) == 1:
+                        noteText.append('')
+                noteList.append(noteText)
+
 
         # get all the course data
         courseTables = sel.xpath('//table[@class="page_table"]/tbody').extract()
         
+        AllDetailCourse = [] # all the teachers' course
         for table in courseTables:
-
+            everyTeacherC = [] # every teacher's course
             s = Selector(text = table)
             trs = s.xpath('//tr').extract()
             for tr in trs:
@@ -121,14 +126,66 @@ class GetTeacherCourseSpider(scrapy.Spider):
                 tmpList.append(location)
 
                 # to know whether every variable is empty
-                detailList = []
+                detailCourse = []
                 for each in tmpList:
                     if not each:
                         each = ''
                     else:
                         each = each[0]
-                    detailList.append(each)
+                    detailCourse.append(each)
 
-                print(detailList)
+                everyTeacherC.append(detailCourse)
+
+            AllDetailCourse.append(everyTeacherC)
+
+        # get department, teacher, gender and title
+        sel = Selector(text = body)
+        temp1 = sel.xpath('//*[@group="group"]/table/tr/td/text()').extract() 
 
 
+        # fill two tables, which will store in the database
+        i = 0
+        # every professor
+        for each in temp1:
+            tables = [] # all the data in every for loop to send to the pipeline 
+
+            each = each.replace(u'\xa0', u'  ')
+            each = each.split('   ')
+            depart = each[0].split('£º')
+            teacher = each[1].split('£º')
+            gender = each[2].split('£º')
+            title = each[3].split('£º')
+
+            # first table
+            profItem = DetailProfItem()
+            profItem['XNXQ'] = self.XNXQ
+            profItem['department'] = depart[1] # department
+            profItem['teacher'] = teacher[1] # teacher
+            profItem['gender'] = gender[1]
+            profItem['title'] = title[1]
+            profItem['note1'] = noteList[i][0]
+            profItem['note2'] = noteList[i][1]
+            tables.append(profItem) # add the first table
+
+            # second table
+            # every professor's courses
+            profCourses = []
+            for j in range(len(AllDetailCourse[i])): # how many course for every professor
+                profCourseItem = DetailProfCourseItem() # every course for every professor
+                profCourseItem['snum'] = AllDetailCourse[i][j][0] # i means i-th professor, j means j-th course, third num means what position of the course
+                profCourseItem['course'] = AllDetailCourse[i][j][1]
+                profCourseItem['credit'] = AllDetailCourse[i][j][2]
+                profCourseItem['teachWay'] = AllDetailCourse[i][j][3]
+                profCourseItem['courseType'] = AllDetailCourse[i][j][4]
+                profCourseItem['classNum'] = AllDetailCourse[i][j][5]
+                profCourseItem['className'] = AllDetailCourse[i][j][6]
+                profCourseItem['stuNum'] = AllDetailCourse[i][j][7]
+                profCourseItem['week'] = AllDetailCourse[i][j][8]
+                profCourseItem['section'] = AllDetailCourse[i][j][9]
+                profCourseItem['location'] = AllDetailCourse[i][j][10]
+                profCourses.append(profCourseItem) # every professor's courses
+
+            tables.append(profCourses) # add the second table
+            print(tables)
+            print('--------------------------------')
+            i += 1
